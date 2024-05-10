f01c706883a2d3a71ecfcb4b90371b6abbaaf998
Fabian Baena <fabian.baena@samanagroup.com>
2024-05-10 13:24:45 -0400

    improve shell handling

--- shell/shell.go
+++ shell/shell.go
@@ -4,6 +4,9 @@ import (
 	"github.com/samanamonitor/gosammwr/protocol"
 	"github.com/beevik/etree"
 	"errors"
+	"strings"
+	"strconv"
+	b64 "encoding/base64"
 )
 
 type Shell struct {
@@ -87,7 +90,7 @@ func (s *Shell) Get(ShellId string) (error, ShellInstance) {
 
 }
 
-func (s *Shell) Create(InputStreams string, OutputStreams string, Name string,
+func (s *Shell) Create(InputStreams []string, OutputStreams []string, Name string,
 		Environment map[string]string, WorkingDirectory string, 
 		Lifetime string, IdleTimeOut string)  (error, ShellInstance) {
 	/*
@@ -97,8 +100,8 @@ func (s *Shell) Create(InputStreams string, OutputStreams string, Name string,
 	resourceURI := "http://schemas.microsoft.com/wbem/wsman/1/windows/shell/cmd"
 
 	shell := etree.NewElement("rsp:Shell")
-	shell.CreateElement("rsp:InputStreams").CreateText("stdin")
-	shell.CreateElement("rsp:OutputStreams").CreateText("stdout stderr")
+	shell.CreateElement("rsp:InputStreams").CreateText(strings.Join(InputStreams, " "))
+	shell.CreateElement("rsp:OutputStreams").CreateText(strings.Join(OutputStreams, " "))
 
 	if len(Name) > 0 {
 		shell.CreateElement("rsp:Name").CreateText(Name)
@@ -181,7 +184,120 @@ func (s *Shell) Command(ShellId string, command []string, SkipCmdShell bool, Con
 		CommandLine.CreateElement("rsp:Arguments").CreateText(command[i])
 	}
 
-	return s.prot.Command(resourceURI, CommandLine, &selectorset, optionset)
+	err, body_str := s.prot.Command(resourceURI, CommandLine, &selectorset, optionset)
+	if err != nil {
+		return err, ""
+	}
+	Body := etree.NewDocument()
+	Body.ReadFromString(body_str)
+	CommandId := Body.FindElement("//CommandId")
+	return nil, CommandId.Text()
+}
+
+const (
+	CommandDone = iota
+	CommandPending = iota
+	CommandRunning = iota
+)
+
+type CommandState struct {
+	StateEnum int
+	ExitCode int
+}
+
+type StreamType struct {
+	End bool
+	Data []byte
+}
+
+func (s *Shell) Receive(ShellId string, CommandId string, Streams []string) (error, CommandState, []StreamType) {
+	resourceURI := "http://schemas.microsoft.com/wbem/wsman/1/windows/shell/cmd"
+
+	selectorset := map[string]string{
+		"ShellId": ShellId,
+	}
+
+	var cs CommandState
+	response := make([]StreamType, len(Streams))
+
+    Receive := etree.NewElement("rsp:Receive")
+    DesiredStream := Receive.CreateElement("rsp:DesiredStream")
+    DesiredStream.CreateAttr("CommandId", CommandId)
+    DesiredStream.CreateText(strings.Join(Streams, " "))
+
+
+    err, body_str := s.prot.Receive(resourceURI, Receive, &selectorset, nil)
+    if err != nil {
+		return err, cs, response
+    }
+	Body := etree.NewDocument()
+	Body.ReadFromString(body_str)
+	for i, stream := range(Streams) {
+		stream_elements := Body.FindElements("//rsp:Stream[@Name='" + stream + "']")
+		for _, element := range(stream_elements) {
+			temp_str2, err := b64.StdEncoding.DecodeString(element.Text())
+			if err != nil {
+				return err, cs, response
+			}
+			response[i].Data = append(response[i].Data, temp_str2...)
+			end_attr := element.SelectAttr("End")
+			if end_attr != nil {
+				if end_attr.Value == "true" {
+					response[i].End = true
+				} else {
+					response[i].End = false
+				}
+			}
+		}
+	}
+	cs_element := Body.FindElement("//rsp:CommandState")
+	state_attr := cs_element.SelectAttr("State")
+	if state_attr == nil {
+		err = errors.New("Invalid response. CommandState is not in response.\n" + body_str)
+		return err, cs, response
+	}
+	if state_attr.Value == "http://schemas.microsoft.com/wbem/wsman/1/windows/shell/CommandState/Done" {
+		cs.StateEnum = CommandDone
+		exit_code_element := cs_element.FindElement("//rsp:ExitCode")
+		if exit_code_element == nil {
+			err = errors.New("Invalid CommandState response. Missing ExitCode.\n" + body_str)
+			return err, cs, response
+		}
+		cs.ExitCode, err = strconv.Atoi(exit_code_element.Text())
+		if err != nil {
+			return err, cs, response
+		}
+	} else if state_attr.Value == "http://schemas.microsoft.com/wbem/wsman/1/windows/shell/CommandState/Pending" {
+		cs.StateEnum = CommandPending
+	} else if state_attr.Value == "http://schemas.microsoft.com/wbem/wsman/1/windows/shell/CommandState/Running" {
+		cs.StateEnum = CommandRunning
+	} else {
+		err = errors.New("Invalid CommandState value.\n" + body_str)
+		return err, cs, response
+	}
+
+	return err, cs, response
+}
+
+func (s *Shell) Send(ShellId string, CommandId string, Data string, Stream string, End bool) (error, string) {
+    resourceURI := "http://schemas.microsoft.com/wbem/wsman/1/windows/shell/cmd"
+
+    selectorset := map[string]string {
+        "ShellId": ShellId,
+    }
+
+    send_element := etree.NewElement("rsp:Send")
+    stream_element := send_element.CreateElement("rsp:Stream")
+    stream_element.CreateAttr("Name", Stream)
+    stream_element.CreateAttr("CommandId", CommandId)
+    stream_element.CreateAttr("End", boolToString[End])
+    stream_element.CreateText(b64.StdEncoding.EncodeToString([]byte(Data)))
+
+    err, response := s.prot.Send(resourceURI, send_element, &selectorset, nil)
+    if err != nil {
+    	return err, ""
+    }
+    return err, response
 }
 
 func (si *ShellInstance) FromEtreeElement(ete *etree.Element) {
